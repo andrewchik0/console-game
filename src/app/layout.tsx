e5af60a108b4698ce573a690124239336d29e4fa
Andrei Vasilev <vasilev6026@gmail.com>
2024-04-09 18:50:13 +0200

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { Metadata, Viewport } from 'next'
 import { Roboto_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 import './globals.css'
 
@@ -40,6 +41,7 @@ export default function RootLayout({
         }}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   )
